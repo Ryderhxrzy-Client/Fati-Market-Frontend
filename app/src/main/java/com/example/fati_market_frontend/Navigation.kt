d28cb044b1c616198b462e0e42f5802ea18d2506
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-26 09:22:55 +0800

    create navigation

---
+++ app/src/main/java/com/example/fati_market_frontend/Navigation.kt
@@ -0,0 +1,32 @@
+package com.example.fati_market_frontend
+
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login",
+        enterTransition = { EnterTransition.None },
+        exitTransition = { ExitTransition.None },
+        popEnterTransition = { EnterTransition.None },
+        popExitTransition = { ExitTransition.None }
+    ) {
+        composable("login") {
+            LoginScreen(navController)
+        }
+        composable("signup") {
+            SignUpScreen(navController)
+        }
+        composable("forgot_password") {
+            ForgotPasswordScreen(navController)
+        }
+    }
+}
