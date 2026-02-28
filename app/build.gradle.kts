0347694e4820bafefcae75c75eead8654a6fa681
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-28 18:49:21 +0800

    build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
