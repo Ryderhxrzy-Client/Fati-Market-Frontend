4a0f4085dc4bb59138f51f0cf861a73fe0fb2ae0
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-03-03 14:00:54 +0800

    Change text to Fati-Market

--- app/src/main/java/com/example/fati_market_frontend/SplashScreen.kt
+++ app/src/main/java/com/example/fati_market_frontend/SplashScreen.kt
@@ -72,7 +72,7 @@ fun SplashScreen(navController: NavController, destination: String = "login") {
             }
 
             Text(
-                text = "Fati-Market ni Ofelia",
+                text = "Fati-Market",
                 color = Color.White,
                 fontSize = 26.sp,
                 fontWeight = FontWeight.Bold,
